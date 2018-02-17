eff19b1e03db1a0866e51a7459f5798cb3e6cb83
Quentin Li <hanyangli42@gmail.com>
2018-02-16 20:07:05 -0600

    HW 3 submitted files

---
+++ hw3_hl43_lzr1/Submitted/hl43_lzr1_code_image/one_vs_all.py
@@ -0,0 +1,80 @@
+from sklearn import linear_model
+import numpy as np
+import utils
+
+class one_vs_allLogisticRegressor:
+
+    def __init__(self,labels):
+        self.theta = None
+        self.labels = labels
+
+    def train(self,X,y,reg):
+        
+        """
+        Use sklearn LogisticRegression for training K classifiers in one-vs-rest mode
+        Read the documentation carefully and choose an appropriate solver. Choose
+        the L2 penalty. Remember that the X data has a column of ones prepended to it.
+        Set the appropriate flag in logisticRegression to cover that.
+        
+        X = m X (d+1) array of training data. Assumes X has an intercept column
+        y = 1 dimensional vector of length m (with K labels)
+        reg = regularization strength
+
+        Computes coefficents for K classifiers: a matrix with (d+1) rows and K columns
+           - one theta of length d for each class
+       """
+        
+        m,dim = X.shape
+        theta_opt = np.zeros((dim,len(self.labels)))
+
+        ###########################################################################
+        # Compute the K logistic regression classifiers                           #
+        # TODO: 7-9 lines of code expected                                        #
+        ###########################################################################
+        # default penaty = l2; X already have the intercept col prpended.
+        # dim = (d +1)
+        for k in range(len(self.labels)):
+            label_k = self.labels[k]
+            #transform the labels
+            y_k = (y == label_k).astype(int)
+            sk_logreg_l2_k = linear_model.LogisticRegression(C=1.0/reg,solver='lbfgs',fit_intercept=False)
+            sk_logreg_l2_k.fit(X,y_k)
+            theta_opt[:,k] = sk_logreg_l2_k.coef_
+
+
+        
+
+        ###########################################################################
+        #                           END OF YOUR CODE                              #
+        ###########################################################################
+        self.theta = theta_opt
+
+    def predict(self,X):
+        """
+        Use the trained weights of this linear classifier to predict labels for'l2'        data points.
+
+        Inputs:
+        - X: m x (d+1) array of training data. 
+
+        Returns:
+        - y_pred: Predicted output for the data in X. y_pred is a 1-dimensional
+          array of length m, and each element is a class label from one of the
+          set of labels -- the one with the highest probability
+        """
+
+        y_pred = np.zeros(X.shape[0])
+
+        ###########################################################################
+        # Compute the predicted outputs for X                                     #
+        # TODO: 1-2 lines of code expected                                        #
+        ###########################################################################
+        # an m by k matrix indicating the probability of each Xi with each class k
+        prob = utils.sigmoid(X.dot(self.theta))
+        # find the indices of the max probability for each sample (each row)
+        y_pred = np.argmax(prob, axis=1)
+
+        ###########################################################################
+        #                           END OF YOUR CODE                              #
+        ###########################################################################
+        return y_pred
+
