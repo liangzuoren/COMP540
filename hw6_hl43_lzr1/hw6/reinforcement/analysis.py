126d6fdcda09f7edb4f3487d0bcc38bd6f668405
= <=>
2018-04-21 17:16:38 -0500

    Add problem 7 part 6

--- hw6_hl43_lzr1/hw6/reinforcement/analysis.py
+++ hw6_hl43_lzr1/hw6/reinforcement/analysis.py
@@ -60,6 +60,7 @@ def question3e():
     # If not possible, return 'NOT POSSIBLE'
 
 def question6():
+    return 'NOT POSSIBLE'
     answerEpsilon = None
     answerLearningRate = None
     return answerEpsilon, answerLearningRate
